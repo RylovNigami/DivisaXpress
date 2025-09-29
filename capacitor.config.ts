3999bd96e2da857bc139ed13128720e0e9525ed3
RylovNigami <mendezr1700@gmail.com>
2025-09-29 14:54:28 -0400

    dist/spa

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'DivisaXpress',
-  webDir: 'www',
+  webDir: 'dist/spa',
   server: {
     androidScheme: 'https',
     iosScheme: 'https',
