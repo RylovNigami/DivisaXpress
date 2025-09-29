49827fdaae9f822192d64e42e05dee58d2e7bbab
RylovNigami <mendezr1700@gmail.com>
2025-09-29 16:24:15 -0400

    Css, cordova configs, and meta tags on index.html

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,14 +3,14 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'DivisaXpress',
-  webDir: 'dist/spa',
+  webDir: 'dist',
   server: {
     androidScheme: 'https',
     iosScheme: 'https',
   },
   ios: {
     scheme: 'App',
-    contentInset: 'always'
+    contentInset: 'automatic'
   }
 };
 
